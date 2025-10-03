cc9c27c83bec57e704198415c8428541202c6d41
Jakub Veverka <veverka.kuba@gmail.com>
2025-10-03 16:21:33 +0200

    test MCP

--- hw-03/main.py
+++ hw-03/main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import asyncio
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.tools import tool, Tool
@@ -8,7 +9,8 @@ from langchain_community.utilities.wolfram_alpha import WolframAlphaAPIWrapper
 from langchain_community.tools.wolfram_alpha.tool import WolframAlphaQueryRun
 from langchain_experimental.utilities import PythonREPL
 from langgraph.prebuilt import create_react_agent
-from visualizer import visualize
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_core.tools import StructuredTool
 
 load_dotenv()
 wolfram = WolframAlphaQueryRun(api_wrapper=WolframAlphaAPIWrapper())
@@ -18,6 +20,12 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
     raise ValueError(
         "GEMINI_API_KEY not found in environment variables. Please set it in your .env file."
+    )
+
+tavily_mcp_url = os.environ.get("TAVILY_MCP_URL")
+if not tavily_mcp_url:
+    raise ValueError(
+        "TAVILY_MCP_URL not found in environment variables. Please set it in your .env file."
     )
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=api_key)
@@ -36,25 +44,33 @@ repl_tool = Tool(
     func=python_repl.run,
 )
 
-tools = [get_food, repl_tool, wolfram]
+# tools = [get_food, repl_tool, wolfram]
 
-# Agent
-graph = create_react_agent(model=llm, tools=tools, prompt="You are a helpful assistant")
 
-# Visualize the graph
-visualize(graph, "graph.png")
+async def make_graph():
+    client = MultiServerMCPClient(
+        {"tavily": {"transport": "streamable_http", "url": tavily_mcp_url}}
+    )
+    tools = await client.get_tools()
+    agent = create_react_agent(
+        model=llm, tools=tools, prompt="You are a helpful assistant"
+    )
+    return agent
 
-# ---------------------------
-# Run the graph
-# MESSAGES are stored ONLY within the graph state !!!!
-# EACH USER INPUT IS A NEW STATE !!!!
-# =>  NO HISTORY for chat interaction !!!!!!
-# ---------------------------
-while True:
-    user_input = input("User: ")
-    if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye!")
-        break
-    for event in graph.stream({"messages": ("user", user_input)}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+
+async def main():
+    graph = await make_graph()
+    print("calling while loop")
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        async for event in graph.astream({"messages": ("user", user_input)}):
+            print("event", event)
+            for value in event.values():
+                print("Assistant:", value["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
